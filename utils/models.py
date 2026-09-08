4dfb8ae9c2ed2f446369a507f5d005fba983154e
AkshayPore <akshaypore10@gmail.com>
2026-09-08 10:54:47 +0530

    Model replacement

--- utils/models.py
+++ utils/models.py
@@ -5,8 +5,10 @@ import os
 load_dotenv()
 
 model=ChatGoogleGenerativeAI(
-    model="gemini-3.6-flash",
-    api_key=os.getenv("GOOGLE_API_KEY")
-)
+    model="gemini-3.5-flash",
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    max_retries=3
+    )
+
 
 structured_model=model.with_structured_output(data)
